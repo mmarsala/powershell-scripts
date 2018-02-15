146a542f6f881aab50c1477161a224d9ffa8a510
Matt Marsala <32486489+mmarsala@users.noreply.github.com>
2018-02-15 14:51:34 -0500

    added new script for getting detailed file and folder info

---
+++ GetDetailedFileAndFolderInfo.ps1
@@ -0,0 +1,135 @@
+﻿# Script to get detailed file and folder info from a path. If the path points to a folder, info for all immediate children will also be reported
+#
+# Requirements: This script must be run under an account with local admin rights on the remote file server.
+#
+# 
+# Usage:
+# .\GetDetailedFileAndFolderInfo.ps1 -path <Path to file or folder>
+#
+# Version: 001
+# Author: Matt Marsala
+
+param
+(
+    [Parameter(Mandatory = $true)]
+    [String]
+    $path
+)
+
+$item = Get-Item $path
+
+# Build a list of streams for the root file or folder
+$rootStreamInfo = Get-Item $path -stream *
+$streamObjs=@()
+
+foreach ($streamItem in $rootStreamInfo) {
+    $streamInfo = @{"StreamName"=$streamItem.Stream; "StreamLength"=$streamItem.Length};
+    $streamObj = New-Object PSObject -Property $streamInfo
+}
+
+# Used to get deailed reparse point information
+$fsutil = "$Env:WinDir\system32\fsutil.exe"
+
+# Check if the root path is a file or folder
+if ($item.PsIsContainer) {
+
+    Write-Host "=====Root Directory [$path]====="
+
+    # If root path is a folder, grab info about the folder and dump it to the console (including list of root streams from above)
+
+    $folderDetails = @{"RootDirectory"=$path; "StreamInfo"=$streamObjs; "Size"=$item.Length; "Attributes"=$item.Attributes;  `
+         "Target"=$item.Target; "LinkType"=$item.LinkType; "Mode"=$item.Mode;  `
+         "LastModTime"=$item.LastWriteTime; "LastAccessedTime"=$item.LastAccessTime; "CreateTime"=$item.CreationTime};
+
+    $folderDetailsObj = New-Object PSObject -Property $folderDetails
+
+    $folderDetailsObj|fl -Property "RootDirectory", "Size", "Attributes", "LinkType", "Target", "Mode", "LastModTime", "LastAccessedTime", "CreateTime", "StreamInfo"
+
+    # Check for link information and if it exists, use fsutil to get more in-depth information on the reparse
+    $linkType = $item.LinkType
+    if (![string]::IsNullOrEmpty($linkType)) {
+        Write-Host "==========Detailed Reparse Info for Root Directory [$path]=========="
+        &$fsutil reparsepoint query $path
+    }
+
+    # Then iterate through all direct children
+    $children = Get-ChildItem -path $path
+
+    Write-Host ""
+
+    foreach ($child in $children) {
+
+        $childPath = "$path\$child"
+
+        # Build a list of streams for the current child
+        $childSteam = Get-Item $child.PSPath -stream *
+        $streamObjs=@()
+
+        foreach ($streamItem in $childSteam) {
+            $streamInfo = @{"StreamName"=$streamItem.Stream; "StreamLength"=$streamItem.Length};
+            $streamObj = New-Object PSObject -Property $streamInfo
+            $streamObjs+=$streamObj
+        }
+
+        # Check if current child is a file or folder, grab its info and dump to the console (including a list of its streams)
+        if ($child.PsIsContainer) {
+            Write-Host "=====Child Directory [$childPath]====="
+
+            $childFolderDetails = @{"ChildDirectory"=$child; "StreamInfo"=$streamObjs; "Size"=$child.Length; "Attributes"=$child.Attributes;  `
+             "Target"=$child.Target; "LinkType"=$child.LinkType; "Mode"=$child.Mode;  `
+             "LastModTime"=$child.LastWriteTime; "LastAccessedTime"=$child.LastAccessTime; "CreateTime"=$child.CreationTime};
+
+            $childFolderDetailsObj = New-Object PSObject -Property $childFolderDetails
+
+            $childFolderDetailsObj|fl -Property "ChildDirectory", "Size", "Attributes", "LinkType", "Target", "Mode", "LastModTime","LastAccessedTime", "CreateTime", "StreamInfo"
+
+            # Check for link information and if it exists, use fsutil to get more in-depth information on the reparse
+            $linkType = $child.LinkType
+            if (![string]::IsNullOrEmpty($linkType)) {
+                Write-Host "==========Detailed Reparse Info for Child Directrory [$childPath]=========="
+                &$fsutil reparsepoint query $childPath
+            }
+
+        } else {
+            Write-Host "=====Child File [$childPath]====="
+
+             $childFileDetails = @{"ChildFile"=$child; "StreamInfo"=$streamObjs; "Size"=$child.Length; "Attributes"=$child.Attributes;  `
+            "Target"=$child.Target; "LinkType"=$child.LinkType; "Mode"=$child.Mode; "Extension"=$child.Extension; `
+             "LastModTime"=$child.LastWriteTime; "LastAccessedTime"=$child.LastAccessTime; "CreateTime"=$child.CreationTime; `
+             "VersionInfo"=$child.VersionInfo};
+
+            $childFileDetailsObj = New-Object PSObject -Property $childFileDetails
+
+            $childFileDetailsObj|fl -Property "ChildFile", "Size", "Attributes", "Extension", "VersionInfo", "LinkType", "Target", "Mode", "LastModTime", "LastAccessedTime", "CreateTime",  "StreamInfo"
+
+            # Check for link information and if it exists, use fsutil to get more in-depth information on the reparse
+            $linkType = $child.LinkType
+            if (![string]::IsNullOrEmpty($linkType)) {
+                Write-Host "==========Detailed Reparse Info for Child File [$childPath]=========="
+                &$fsutil reparsepoint query $childPath
+            }
+        }
+
+        Write-Host ""
+    }
+
+} else {
+
+    # If root path is a file, grab info about the file and dump it to the console (including list of root streams from above)
+
+    $fileDetails = @{"File"=$path; "StreamInfo"=$streamObjs; "Size"=$item.Length; "Attributes"=$item.Attributes;  `
+         "Target"=$item.Target; "LinkType"=$item.LinkType; "Mode"=$item.Mode; "Extension"=$item.Extension; `
+         "LastModTime"=$item.LastWriteTime; "LastAccessedTime"=$item.LastAccessTime; "CreateTime"=$item.CreationTime; `
+         "VersionInfo"=$item.VersionInfo};
+
+    $fileDetailsObj = New-Object PSObject -Property $fileDetails
+
+    $fileDetailsObj|fl -Property "Path", "Size", "Attributes","Extension", "VersionInfo", "LinkType", "Target", "Mode", "LastModTime","LastAccessedTime", "CreateTime", "StreamInfo"
+
+    # Check for link information and if it exists, use fsutil to get more in-depth information on the reparse
+    $linkType = $item.LinkType
+    if (![string]::IsNullOrEmpty($linkType)) {
+        Write-Host "=====Detailed Reparse Info for Root File [$path]====="
+        &$fsutil reparsepoint query $path
+    }
+}
--- README.md
+++ README.md
@@ -8,5 +8,11 @@ A growing repository of simple PowerShell scripts to use in various networking a
 .\GetLocalGroupsAndUsers.ps1 -fileserver <NAME, IP, or FQDN OF FILE SERVER>
 ```
 
+`GetDetailedFileAndFolderInfo.ps1` - A script to retrieve detailed information on a specified file or folder. Information includes reparse details and alternate data streams. If the script is pointed at a folder, it will return information on the root folder as well as for all immediate children.
+```
+.\GetDetailedFileAndFolderInfo.ps1 -path <LOCAL OR REMOTE PATH TO A FILE OR FOLDER>
+```
+
+
 ## Disclaimer
 This repository does not represent an official repository for Peer Software or Microsoft. All code and information in this repository is provided as is.
